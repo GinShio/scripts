ba5b5c90d63daab1471e41b60729cb6b315f2720
GinShio <ginshio78@gmail.com>
2026-02-13 17:57:44 +0800

    setup(opensuse): Update packages

--- setup/usages/common/opensuse/07-virtualization.sh
+++ setup/usages/common/opensuse/07-virtualization.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 #@tags: usage:common, scope:system, os:opensuse
 
-# Virtualization & Containerization & Cross compilation
+# Cross compilation
+# -----------------------------------------------------------------------------
+sudo -E zypper in -y \
+    cross-aarch64-binutils cross-aarch64-gcc14 cross-aarch64-linux-glibc-devel \
+    cross-arm-binutils cross-arm-gcc14 cross-arm-linux-glibc-devel \
+    cross-ppc64-binutils cross-ppc64-gcc14 cross-ppc64-linux-glibc-devel \
+    cross-ppc64le-binutils cross-ppc64le-gcc14 cross-ppc64le-linux-glibc-devel \
+    cross-riscv64-binutils cross-riscv64-gcc14 cross-riscv64-linux-glibc-devel \
+    cross-s390x-binutils cross-s390x-gcc14 cross-s390x-linux-glibc-devel
+
+# Virtualization
 # -----------------------------------------------------------------------------
 sudo -E zypper in -y -t pattern kvm_tools
 sudo -E zypper in -y \
-    buildah cross-{aarch64,arm,ppc64,ppc64le,riscv64,s390x}-{binutils,gcc14,linux-glibc-devel} crun crun-vm libvirt \
-    libvirt-daemon-lxc libvirt-dbus libvirt-doc lxc lxd podman podman-docker qemu{,-extra,-doc,-lang} \
-    qemu-{arm,ppc,x86} qemu-linux-user qemu-vhost-user-gpu
+    libvirt libvirt-daemon-lxc libvirt-dbus libvirt-doc qemu qemu-arm qemu-extra qemu-doc qemu-lang qemu-linux-user \
+    qemu-ppc qemu-vhost-user-gpu qemu-x86
+
+# Containerization
+# -----------------------------------------------------------------------------
+sudo -E zypper in -y \
+    buildah crun crun-vm libcgroup-tools lxc lxd podman podman-docker \
+
 # riscv64-suse-linux-gcc -march=rv64gc riscv.c
 # clang --target=riscv64-suse-linux --sysroot=/usr/riscv64-suse-linux/sys-root -mcpu=generic-rv64 -march=rv64g riscv.c
 # QEMU_LD_PREFIX=/usr/riscv64-suse-linux/sys-root c.out
--- setup/usages/common/opensuse/09-amdgpu.sh
+++ setup/usages/common/opensuse/09-amdgpu.sh
@@ -2,11 +2,14 @@
 #@tags: usage:common, scope:system, os:opensuse, gpu:amd
 
 sudo zypper in -y \
-    libvulkan_radeon libvulkan_radeon-32bit xf86-video-amdgpu
+    kernel-firmware-amdgpu libdrm_amdgpu1 libdrm_amdgpu1-32bit libvulkan_radeon libvulkan_radeon-32bit \
+    radeontop radeontop-lang xf86-video-amdgpu
 
 sudo zypper ar -fcg obs://science:GPU:ROCm openSUSE:ROCm
 sudo zypper ref
 
 sudo -E zypper in -y \
-    kernel-firmware-amdgpu libdrm_amdgpu1 libdrm_amdgpu1-32bit libvulkan_radeon libvulkan_radeon-32bit \
-    mojo-shader-amdgpu-pro-precompile radeontop
+    amdsmi hipcc 'libhip*' 'librocalution*' 'librocblas*' 'librocfft*' 'librocm-core*' rocminfo rocm-clang \
+    rocm-clang-devel rocm-clang-libs rocm-clang-runtime-devel rocm-cmake rocm-compilersupport-macros rocm-device-libs \
+    rocm-hip{,-devel} rocm-libc++-devel rocm-lld rocm-llvm rocm-llvm-devel rocm-llvm-libs rocm-llvm-static rocm-smi \
+    roctracer
